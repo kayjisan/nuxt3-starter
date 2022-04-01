89efe944e7f8db64cdc868ace0519ec4026a7cbd
Erkan <erkan.guemues@gmx.de>
2022-04-01 20:03:25 +0200

    Fix color mode issue

--- components/ColorMode.vue
+++ components/ColorMode.vue
@@ -1,12 +1,12 @@
 <template>
-  <button @click="next()">
+  <button @click="mode = mode === 'dark' ? 'light' : 'dark'">
     <SunIcon v-if="mode === 'dark'" class="h-6 w-6" aria-hidden="true" />
-    <MoonIcon v-if="mode === 'light'" class="h-6 w-6" aria-hidden="true" />
+    <MoonIcon v-else class="h-6 w-6" aria-hidden="true" />
   </button>
 </template>
 
 <script>
-import { useColorMode, useCycleList } from '@vueuse/core'
+import { useColorMode } from '@vueuse/core'
 import { MoonIcon, SunIcon } from '@heroicons/vue/outline'
 
 export default {
@@ -18,11 +18,8 @@ export default {
   setup() {
     const mode = useColorMode()
 
-    const { next } = useCycleList(['dark', 'light'], { initialValue: mode })
-
     return {
       mode,
-      next
     };
   },
 };
